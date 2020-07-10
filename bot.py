845476305b0882ca3fee99c6568607cf4078a2c1
lynnlo <tharlynnhtet.ong@gmail.com>
2020-07-09 21:04:59 -0700

    html console and status

--- bot.py
+++ bot.py
@@ -4,13 +4,12 @@
 # Imports
 import discord
 import json
-import tkinter
-from tkinter import font
+import time
+import sys
 
 # Constants
 Key           = open("key.txt", "r").read()
-TkMain        = tkinter.Tk()
-StatusChannel = 0
+StatusChannel = 603079732015136768
 
 # Variables
 prefix = "$"
@@ -28,11 +27,15 @@ async def Help(message, args):
 async def Kill(message, args):
     await bot.close()
 
+async def UpdateStatus(message, args):
+    await bot.get_channel(StatusChannel).send("Status")
+
 # Command : Function
 Commands = {
 "ping" : Ping,
 "help" : Help,
 "kill" : Kill,
+"stat" : UpdateStatus,
 }
 
 # Command : [[Aliases], "Description"]
@@ -40,6 +43,7 @@ CommandInfo = {
 "ping" : [["latency"], "Send back the latency of the bot."],
 "help" : [["cmds, commands"], "Displays all of the commands."],
 "kill" : [["stop", "close"], "Stops the bot."],
+"stat" : [["status"], "Get the status of the game."]
 }
 
 # Events
@@ -58,64 +62,10 @@ async def on_ready():
 
 # Gui Functions
 
-def SetChannel():
-    StatusChannel = int(ChannelBox.get())
+def SetChannel(Value):
+    StatusChannel = Value
 
-def SetKey():
-    Key = KeyBox.get()
+def SetKey(Value):
+    Key = Value
 
-def RunBot():
-    TkMain.destroy()
-    bot.run(Key)
-
-def Break(lines = 2):
-    Break = tkinter.Label(TkMain, text = "")
-    Break.config(width = 80, height = lines)
-    Break.pack()
-
-# Gui
-TkMain.title("CodeCup Discord Helper")
-TkMain.geometry("600x450")
-
-Font = tkinter.font.nametofont("TkDefaultFont")
-Font.config(size = 12)
-
-Title              = tkinter.Label(TkMain, text = "CodeCup Discord Helper")
-KeyLabel           = tkinter.Label(TkMain, text = "Discord Key")
-KeyBox             = tkinter.Entry(TkMain, justify = "center")
-KeySetButton       = tkinter.Button(TkMain, text = "Set Key", command = SetKey)
-ChannelLabel       = tkinter.Label(TkMain, text = "Status Channel")
-ChannelBox         = tkinter.Entry(TkMain, justify = "center")
-ChannelSetButton   = tkinter.Button(TkMain, text = "Set Channel", command = SetChannel)
-RunButton          = tkinter.Button(TkMain, text = "Run", command = RunBot)
-RunWarning         = tkinter.Label(TkMain, text = "As of right now, running will close the window but the discord bot will still run.")
-
-Title.config(width = 50)
-KeyLabel.config(width = 30)
-KeyBox.config(width = 70)
-KeySetButton.config(width = 10)
-ChannelLabel.config(width = 30)
-ChannelBox.config(width = 30)
-ChannelSetButton.config(width = 10)
-RunButton.config(width = 30)
-RunWarning.config(width = 80)
-
-Title.pack()
-Break()
-KeyLabel.pack()
-KeyBox.pack()
-Break(1)
-KeySetButton.pack()
-Break()
-ChannelLabel.pack()
-ChannelBox.pack()
-Break(1)
-ChannelSetButton.pack()
-Break()
-RunButton.pack()
-RunWarning.pack()
-
-# Run
-KeyBox.insert(0, Key)
-ChannelBox.insert(0, str(StatusChannel))
-tkinter.mainloop()
+bot.run(Key)
