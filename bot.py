dc10c60bb3c667844b773e7ec4ec190bfa59b994
lynnlo <tharlynnhtet.ong@gmail.com>
2020-07-31 19:48:45 -0700

    Switched to Enviroment Variables.

--- bot.py
+++ bot.py
@@ -3,33 +3,25 @@
 # Imports
 import threading
 import requests
+import os
 
 from src.key import *
 from src.ctf import *
 from src.dsc import *
 
-# Settings
-info = []
-for x in open("info", "r").read().split("\n"):
-    if x.startswith("#") or x == "":
-        continue
-    else:
-        info.append(x)
-
 # Info
-prefix = info[0]
-status = info[1]
-state  = info[2]
+prefix = os.getenv("PREFIX")
+status = os.getenv("STATUS")
 
 # Channels
-CommandChannel = info[3]
-UpdateChannel  = info[4]
+CommandChannel = os.getenv("COMMAND_CHANNEL")
+UpdateChannel  = os.getenv("UPDATE_CHANNEL")
 
 # Credentials
-DiscordKey   = info[5]
-CTFDUsername = info[6]
-CTFDPassword = info[7]
-CTFDURL      = info[8]
+DiscordKey   = os.getenv("BOT_TOKEN")
+CTFDUsername = os.getenv("CTFD_USERNAME")
+CTFDPassword = os.getenv("CTFD_PASSWORD")
+CTFDURL      = os.getenv("CODECUP_LOGIN_URL")
 
 # API
 bot = Bot(command_prefix = prefix, description = "A bot that helps out with Code Cup.")
@@ -39,31 +31,8 @@ api = login(CTFDUsername, CTFDPassword, CTFDURL)
 def Run():
     global state
     print("Running : codecup-discord-help")
-    print("State : " + state)
-    if state.lower() == "active":
-        runCTF(api)
-        runDiscord(bot, prefix, DiscordKey, status)
-        threading.Thread(target = runDymanic).start()
-    if state.lower() == "passive":
-        runCTF(api)
-        runDiscord(bot, prefix, DiscordKey, status)
-        threading.Thread(target = runDymanic).start()
-    if state.lower() == "testing":
-        c = ""
-        while c != "quit":
-            c = input(" > ")
-            if c == "active":
-                state = "Active"
-                runCTF(api)
-                runDiscord(bot, prefix, DiscordKey, status)
-                threading.Thread(target = runDymanic).start()
-            elif c == "passive":
-                state = "Passive"
-                print("State set to : Passive")
-            else:
-                try:
-                    print(eval(c))
-                except Exception as e:
-                    print(e)
-            
+    runCTF(api)
+    runDiscord(bot, prefix, DiscordKey, status)
+    threading.Thread(target = runDymanic).start()
+
 Run()
